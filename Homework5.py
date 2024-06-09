a29da80a7b532ad93f10ccf671e87223595cb000
Kateone <qetichabukiani26@gmail.com>
2024-06-09 21:46:07 +0400

    fix issues for Homework5

--- Homework5.py
+++ Homework5.py
@@ -1,22 +1,22 @@
-class car:
+class Car:
     wheel = 4
     steering_Wheel = 1
     engine = 1
     gas_pedal = 1
 
-    def __init__(self, seats, fit, isEco = True):
+    def __init__(self, seats, fit, iseco=True):
         self.seats = seats
         self.fit = fit
-        if isEco == False:
-            self.isEco = "is not eco friendly"
+        if iseco:
+            self.iseco = "is eco friendly"
         else:
-            self.isEco = "is eco friendly"
+            self.iseco = "is not eco friendly"
 
-    def carIntroduction(self):
-        print(f"Hi my car has {self.seats} seats, it can fit {self.fit} person and it {self.isEco}")
+    def car_introduction(self):
+        print(f"Hi my car has {self.seats} seats, it can fit {self.fit} person and it {self.iseco}")
 
     @staticmethod
-    def person_Per_Family(*numbers):
+    def person_per_family(*numbers):
         return f"We're looking for a car that will fit {sum(numbers)} passengers"
 
     @classmethod
@@ -24,28 +24,29 @@ class car:
         print(f"My car only has {cls.wheel} wheels")
 
     @classmethod
-    def gasPedanNumber(cls, breaks, teaching = False):
-        if teaching == False:
+    def gas_pedal_number(cls, breaks, teaching=False):
+        if not teaching:
             print(f"My car only has {cls.gas_pedal} gas pedal and {breaks} break pedal as it isn't a teaching car ")
         else:
             print(f"My car only has {cls.gas_pedal} gas pedal and {breaks} break pedal as it is a teaching car ")
 
     def __repr__(self):
-        return f"(Seat number: {self.seats}, Fits {self.fit} person, is eco: {self.isEco})"
+        return f"(Seat number: {self.seats}, Fits {self.fit} person, is eco: {self.iseco})"
 
-car1 = car(4,5, False)
-car2 = car(7, 8, True)
-car3 = car(22,23,False)
-car4 = car(44,50,True)
-car5 = car(18,20,True)
+
+car1 = Car(4, 5, False)
+car2 = Car(7, 8, True)
+car3 = Car(22, 23, False)
+car4 = Car(44, 50, True)
+car5 = Car(18, 20, True)
 
 car4.wheel = 8
 car5.wheel = 6
 
-print(car.person_Per_Family(5,4,9))
-car.wheel_number()
-car1.carIntroduction()
-car.gasPedanNumber(1, False)
+print(Car.person_per_family(5, 4, 9))
+Car.wheel_number()
+car1.car_introduction()
+Car.gas_pedal_number(2, True)
 print(car1, car2)
 
 print(car4.wheel)
